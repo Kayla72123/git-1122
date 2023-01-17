1d578c074deaa3f882774b55cefc4b711dd6d32f
rishyak <hello@rishyak.com>
2023-01-17 13:37:14 -0500

    Add user input
    Signed-off-by: rishyak <hello@rishyak.com>

--- rps.py
+++ rps.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python3
+
 from random import choice
 
-def game():
+objects = ("rock", "paper", "scissors")
+
+
+def get_input() -> str:
+    user_input = input('Please enter your choice or "Q" or "q" to quit: ')
+
+    user_choice = user_input.strip().lower()
+
+    return user_choice
+
+
+def game() -> None:
     pass
 
-def main():
+
+def main() -> None:
     pass
 
 
