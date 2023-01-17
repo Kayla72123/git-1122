fa827388e449944452d7e8038d3fe17136d69280
rishyak <hello@rishyak.com>
2023-01-17 13:37:40 -0500

    Add scoring system
    Signed-off-by: rishyak <hello@rishyak.com>

--- rps.py
+++ rps.py
@@ -4,6 +4,19 @@ from random import choice
 
 objects = ("rock", "paper", "scissors")
 
+user_score = 0
+computer_score = 0
+
+
+def user_won():
+    global user_score
+    user_score += 1
+
+
+def computer_won():
+    global computer_score
+    computer_score += 0
+
 
 def get_input() -> str:
     user_input = input('Please enter your choice or "Q" or "q" to quit: ')
